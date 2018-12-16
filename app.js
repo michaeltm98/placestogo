ed2447e44c7416e65eea302aca030f72c6dd2919
EC2 Default User <ec2-user@ip-172-31-42-138.ec2.internal>
2018-12-16 16:12:15 +0000

    fixed for server compatibility

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
-
+const port = 9000;
+var http = require('http');
+/*http.createServer(function(req,res){ 
+  res.writeHead(200,{'Content-Type':'text/plain'});
+  res.end('Hello world!\n');
+}).listen(port);
+console.log('Listening on port',port);
+*/
 var places = [
     {name: "Machu Picchu", image: "https://media-cdn.tripadvisor.com/media/photo-s/02/5c/5c/8a/a-voir-absolument.jpg"},
     {name: "Spain", image: "https://exclusivesmedia.webjet.com.au/uploads/2018/08/Highlights-Spain-2.jpg"},
@@ -20,8 +27,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function() {
-    console.log("Server is listening on port 3000...")
+app.listen(8080, function() {
+    console.log("Server is listening on port 9000...")
 });
 
 app.get('/', function(req, res) {
