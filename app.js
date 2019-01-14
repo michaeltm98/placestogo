d651fbb1f829d92f673b86953b249f45cd5ebe78
Michael Madden <michaeltm98@gmail.con>
2019-01-14 18:56:03 -0500

    added EDIT, DELETE, added responsiveness to new post page

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
+const methodOverride = require("method-override");
 
 mongoose.connect('mongodb://localhost:27017/placestogo', {useNewUrlParser: true});
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 var places = [
     {name: "Machu Picchu", image: "https://media-cdn.tripadvisor.com/media/photo-s/02/5c/5c/8a/a-voir-absolument.jpg"},
@@ -30,18 +32,7 @@ var placeSchema = new mongoose.Schema({
 
 var Place = mongoose.model("Place", placeSchema);
 
-function createPlace() {
-    Place.create({
-        name: "TestPlace",
-        image: "https://www.telegraph.co.uk/content/dam/Travel/ski/K2-mountain-Andrzej-Bargiel-first-ski-descent-by-Piotr-Pawlus-Red-Bull-Content-Pool.jpg?imwidth=1240",
-        description: "testplace"
-    });
-}
 
-
-app.listen(8080, function() {
-    console.log("Server is listening on port 8080...")
-});
 
 app.get('/', function(req, res) {
     res.redirect('/places');
@@ -72,12 +63,51 @@ app.get('/places/:id', function(req, res) {
         } else {
             res.render("show", {place, place});
         }
+    });
+});
+
+app.get('/places/:id/edit', function(req, res) {
+    var id = req.params.id;
+    Place.findById(req.params.id, function(err, place) {
+        if(err) {
+            console.log(err);
+            res.redirect('/places');
+        }
+        else {
+            res.render('edit', {place: place});
+        }
     })
 })
 
+app.put('/places/:id', function(req, res) {
+
+    Place.findByIdAndUpdate(req.params.id, req.body.place, function(err, place) {
+        if(err) {
+            console.log(`Error updating place ${err}`);
+            res.redirect('/blogs');
+            
+        }
+        else {
+            res.redirect(`/places/${req.params.id}`);
+        }
+    });
+});
+
+
+app.delete('/places/:id', function(req, res) {
+    Place.findByIdAndRemove(req.params.id, function(err) {
+        if(err) {
+            console.log(err);
+            res.redirect('/places');
+        }
+        else {
+            res.redirect('/places');
+        }
+    })
+})
 
 //create
-app.post('/place', function(req, res) {
+app.post('/places', function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
@@ -91,4 +121,10 @@ app.post('/place', function(req, res) {
             res.redirect("/places");
         }
     });
+});
+
+
+
+app.listen(8080, function() {
+    console.log("Server is listening on port 8080...")
 });
